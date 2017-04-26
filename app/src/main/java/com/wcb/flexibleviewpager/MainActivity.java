158708e73613c9ae9d102f882c5f04bfabc607de
xubo <1623881360@qq.com>
2017-04-26 16:57:38 +0800

    code

---
+++ app/src/main/java/com/wcb/flexibleviewpager/MainActivity.java
@@ -0,0 +1,54 @@
+package com.wcb.flexibleviewpager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.wcb.flexibleviewpager.adapter.MyViewPagerAdapter;
+import com.wcb.flexibleviewpager.view.FlexibleViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements FlexibleViewPager.OnRefreshListener {
+    private FlexibleViewPager viewPager;
+    private MyViewPagerAdapter adapter;
+    private List<View> list = new ArrayList<>();
+    private int[] imgs = {R.mipmap.pic1, R.mipmap.pic2, R.mipmap.pic3};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = (FlexibleViewPager) findViewById(R.id.viewPager);
+        initData();
+        adapter = new MyViewPagerAdapter(list);
+        viewPager.setAdapter(adapter);
+        viewPager.setOnRefreshListener(this);
+    }
+
+    private void initData() {
+        for (int img : imgs) {
+            ImageView imageView = new ImageView(this);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setImageResource(img);
+            list.add(imageView);
+        }
+    }
+
+    @Override
+    public void onRefresh() {
+        showToast("刷新");
+    }
+
+    @Override
+    public void onLoadMore() {
+        showToast("加载更多");
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/wcb/flexibleviewpager/adapter/MyViewPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.wcb.flexibleviewpager.adapter;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/4/26.
+ */
+
+public class MyViewPagerAdapter extends PagerAdapter {
+    private List<View> list;
+
+    public MyViewPagerAdapter(List<View> list) {
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view = list.get(position);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView(list.get(position));
+    }
+}
